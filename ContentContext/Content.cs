eb3e14b6894c8b4215c042abc5f7ce9d1d574fad
Lucas Vilela <eu@vilela.dev>
2023-09-30 23:30:28 -0300

    Changing Id to Guid and making content class abstract

--- ContentContext/Content.cs
+++ ContentContext/Content.cs
@@ -1,8 +1,13 @@
 namespace BaltaFundamentosPOO.ContentContext;
 
-public class Content
+public abstract class Content
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public string Title { get; set; }
     public string Url { get; set; }
+
+    public Content()
+    {
+        Id = Guid.NewGuid();
+    }
 }
